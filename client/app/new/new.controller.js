bf868d80a6dcabe91bd304d611c3dc99de42328c
Dan Hems <dan.hems@telegraph.co.uk>
2015-03-24 14:39:24 +0000

    Take latlng from autocomplete directive instead of geocoder service to avoid unnecessary work and extra request.

--- client/app/new/new.controller.js
+++ client/app/new/new.controller.js
@@ -3,7 +3,10 @@
 angular.module('Interviews')
   .controller('NewCtrl', ['$scope', '$http', 'geocoder', function ($scope, $http, geocoder) {
     
+    // placeholder object for form values
     $scope.interview = {};
+    // placeholder object for google location data provided by autocomplete directive
+    $scope.placeData = {};
 
     $scope.newinterviewForm = $('.new-interview');
 
@@ -13,35 +16,25 @@ angular.module('Interviews')
 
     $scope.save = function(){
 
-      geocoder.geocodeAddress( $scope.interview.address ) 
-      .then( function(data){
-        $scope.interview.location = {
-          lat: data.lat,
-          lng: data.lng
-        };
-        
-        // console.log( $scope.interview.location );
+      $scope.interview.location = {
+        lat: $scope.placeData.geometry.location.k,
+        lng: $scope.placeData.geometry.location.D
+      };
 
-        $scope.interview.dateCreated = new Date();
+      $scope.interview.dateCreated = new Date();
 
-        $scope.isSubmitted = true;
-        
-        // send to server
-        //*
-        $http.post('/api/interviews', $scope.interview)
-          .success( function(response){
-            if(response){
-              $scope.submissionSuccess = true;
-              console.log(response);
-            }
-          })
-          .error( function(err){
-            $scope.submissionError = true;
-            console.log(err);
-          });
-        //*/
-      });
-
+      $scope.isSubmitted = true;
+      
+      $http.post('/api/interviews', $scope.interview)
+        .success( function(response){
+          if(response){
+            $scope.submissionSuccess = true;
+          }
+        })
+        .error( function(err){
+          $scope.submissionError = true;
+          console.log(err);
+        });
     };
 
     $scope.resetForm = function(){
