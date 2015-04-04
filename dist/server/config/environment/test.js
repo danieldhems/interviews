3086fb8d7f5b00553f684e9d87690e4827589332
Dan <danhems87@gmail.com>
2015-04-05 00:53:03 +0100

    Add dist dir for deployment to heroku.

---
+++ dist/server/config/environment/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Test specific configuration
+// ===========================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://admin:Cat09021988@ds039321.mongolab.com:39321/heroku_app35447905'
+  }
+};
